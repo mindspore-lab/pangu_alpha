12b4d144fdc9ba1435477928e17f2887aaacba18
chenzeng <chenzeng8@huawei.com>
2023-02-02 10:52:59 +0800

    afqmc eval and infer

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: UTF-8 -*-
-from src.model.pangu_alpha import PanguAlpha, PanguAlpha_Model, EvalNet_p, PanguAlphaWithLoss2,\
-    PanguAlpha_BC, PanguAlphaWithLoss
+from src.model.pangu_alpha import PanguAlpha, PanguAlpha_Model, EvalNet_p, PanguAlphaWithLoss2,PanguAlpha_BC, \
+    PanguAlphaWithLoss
 from src.model.pangu_alpha_config import PanguAlphaConfig, set_parse
 from src.model.base_modules import LearningRate, TrainingMonitor, CrossEntropyLoss, VirtualDatasetOneInputCell
