319e1b0a395368b73f069d20b3f38e77ead386de
chenzeng8 <122774235+chenzeng8@users.noreply.github.com>
2023-02-06 10:23:38 +0800

    afqmc finetune (#3)
    * readme 文档
    
    * pangu_alpha模型
    
    * afqmc eval and infer
    
    * afqmc eval and infer
    
    * afqmc eval and infer
    
    * afqmc eval and infer
    
    * afqmc eval and infer
    
    * afqmc finetune
    
    * afqmc finetune
    
    * afqmc finetune

--- src/afqmc/scripts/run_finetune_dist.sh
+++ src/afqmc/scripts/run_finetune_dist.sh
@@ -16,9 +16,9 @@
 
 echo "=============================================================================================================="
 echo "Please run the script as: "
-echo "bash run_eval_dist.sh [RANK_TABLE_FILE] [DEVICE_NUM] [DEVICE_START]"
+echo "bash run_finetune_dist.sh [RANK_TABLE_FILE] [DEVICE_NUM] [DEVICE_START]"
 echo "for example:"
-echo "bash run_eval_dist_13B.sh /path/hccl.json 4 0"
+echo "bash run_finetune_dist.sh /path/hccl.json 4 0"
 echo "It is better to use absolute path."
 echo "=============================================================================================================="
 
@@ -30,20 +30,20 @@ export RANK_SIZE=$RANK_SIZE
 export RANK_TABLE_FILE=$1
 
 data_path=/store0/pangu_alpha/afqmc/dataset/
-output_path=/store0/pangu_alpha/output_pangu/
-ckpt_path=/store0/pangu_alpha/pretrained_models/13B/
-model_config_path=${LOCAL_PATH}/configs/eval_model_config_pangu_13B_afqmc.yaml
+output_path=/store0/pangu_alpha/output_pangu
+pretrained_model_path=/store0/pangu_alpha/pretrained_models/2B6/
+finetune_model_config=${LOCAL_PATH}/configs/finetune_model_config_pangu_afqmc.yaml
 
 for ((i = 0; i < ${RANK_SIZE}; i++)); do
   export RANK_ID=$i
   export DEVICE_ID=$((i + DEVICE_START))
   echo DEVICE_ID=$DEVICE_ID
-  echo RANK_ID="$RANK_ID"
-  # evaluate
-  tk evaluate --quiet \
-           --boot_file_path="${LOCAL_PATH}"/src/evaluate_main.py \
-           --data_path=$data_path \
-           --output_path=$output_path \
-           --ckpt_path=$ckpt_path \
-           --model_config_path="$model_config_path" &
+  echo RANK_ID=$RANK_ID
+  # finetune
+  tk finetune --quiet \
+              --boot_file_path=${LOCAL_PATH}/main/finetune_main.py \
+              --data_path=$data_path \
+              --output_path=$output_path \
+              --pretrained_model_path=$pretrained_model_path \
+              --model_config_path=$finetune_model_config &
 done
