4e8afa39c41dcb589533b96a67dddaf53c75c900
chenzeng <chenzeng8@huawei.com>
2023-02-25 15:24:15 +0800

    cmrc2018 eval

--- src/tnews/scripts/run_eval_dist.sh
+++ src/tnews/scripts/run_eval_dist.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# -*- coding: UTF-8 -*-
-# Copyright 2022 Huawei Technologies Co., Ltd
+# Copyright 2023 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 #!/bin/bash
